0b6777349f6affd5073bf528240899ccddc08d6a
codymj <codyj@protonmail.com>
2024-03-28 06:05:58 -0500

    minor changes

--- src/settings.cpp
+++ src/settings.cpp
@@ -5,7 +5,7 @@
 void Settings::load(const std::string& config_file) {
 	// load configuration settings from file
 	simdjson::ondemand::parser parser;
-	simdjson::padded_string json = simdjson::padded_string::load(config_file);
+	const simdjson::padded_string json = simdjson::padded_string::load(config_file);
 	simdjson::ondemand::document doc = parser.iterate(json);
 
 	try {
@@ -29,30 +29,30 @@ void Settings::load(const std::string& config_file) {
 		properties["service_version"] = std::string_view(doc["service"]["version"]);
 
 		// server.port
-		if (int64_t(doc["server"]["port"])<0 || int64_t(doc["server"]["port"])>65535) {
+		if (static_cast<int64_t>(doc["server"]["port"])<0 || static_cast<int64_t>(doc["server"]["port"])>65535) {
 			throw std::invalid_argument("server.port must be between 0 and 65535");
 		}
-		properties["server_port"] = (int)int64_t(doc["server"]["port"]);
+		properties["server_port"] = static_cast<int64_t>(doc["server"]["port"]);
 
 		// server.read_timeout
-		if (int64_t(doc["server"]["read_timeout"])<0) {
+		if (static_cast<int64_t>(doc["server"]["read_timeout"])<0) {
 			throw std::invalid_argument("server.read_timeout must be 0 seconds or more");
 		}
-		properties["server_read_timeout"] = (int)int64_t(doc["server"]["read_timeout"]);
+		properties["server_read_timeout"] = static_cast<int64_t>(doc["server"]["read_timeout"]);
 
 		// server.write_timeout
-		if (int64_t(doc["server"]["write_timeout"])<0) {
+		if (static_cast<int64_t>(doc["server"]["write_timeout"])<0) {
 			throw std::invalid_argument(
 				"server.write_timeout must be 0 seconds or more");
 		}
-		properties["server_write_timeout"] = (int)int64_t(doc["server"]["write_timeout"]);
+		properties["server_write_timeout"] = static_cast<int64_t>(doc["server"]["write_timeout"]);
 
 		// server.idle_timeout
-		if (int64_t(doc["server"]["idle_timeout"])<0) {
+		if (static_cast<int64_t>(doc["server"]["idle_timeout"])<0) {
 			throw std::invalid_argument(
 				"server.idle_timeout must be 0 seconds or more");
 		}
-		properties["server_idle_timeout"] = (int)int64_t(doc["server"]["idle_timeout"]);
+		properties["server_idle_timeout"] = static_cast<int64_t>(doc["server"]["idle_timeout"]);
 
 		// database.host
 		if (std::string_view(doc["database"]["host"]).empty()) {
@@ -61,10 +61,10 @@ void Settings::load(const std::string& config_file) {
 		properties["database_host"] = std::string_view(doc["database"]["host"]);
 
 		// database.port
-		if (int64_t(doc["database"]["port"])<0 || int64_t(doc["database"]["port"])>65535) {
+		if (static_cast<int64_t>(doc["database"]["port"])<0 || static_cast<int64_t>(doc["database"]["port"])>65535) {
 			throw std::invalid_argument("database.port must be between 0 and 65535");
 		}
-		properties["database_port"] = (int)int64_t(doc["database"]["port"]);
+		properties["database_port"] = static_cast<int64_t>(doc["database"]["port"]);
 
 		// database.user
 		if (std::string_view(doc["database"]["user"]).empty()) {
@@ -73,11 +73,11 @@ void Settings::load(const std::string& config_file) {
 		properties["database_user"] = std::string_view(doc["database"]["user"]);
 
 		// database.password
-		std::string_view password_env{std::string_view(doc["database"]["password"])};
+		auto password_env{std::string_view(doc["database"]["password"])};
 		if (std::string_view(doc["database"]["password"]).empty()) {
 			throw std::invalid_argument("database.password cannot be empty");
 		}
-		std::string tmp{password_env.begin(), password_env.end()};
+		const std::string tmp{password_env.begin(), password_env.end()};
 		const char* password = std::getenv(tmp.c_str());
 		if (password == nullptr) {
 			throw std::invalid_argument("Please set a database password environment variable");
@@ -91,24 +91,24 @@ void Settings::load(const std::string& config_file) {
 		properties["database_name"] = std::string_view(doc["database"]["name"]);
 
 		// database.connection_max_lifetime
-		if (int64_t(doc["database"]["connection_max_lifetime"])<0) {
+		if (static_cast<int64_t>(doc["database"]["connection_max_lifetime"])<0) {
 			throw std::invalid_argument("database.connection_max_lifetime must be 0 or greater");
 		}
-		properties["database_connection_max_lifetime"] = (int)int64_t(doc["database"]["connection_max_lifetime"]);
+		properties["database_connection_max_lifetime"] = static_cast<int64_t>(doc["database"]["connection_max_lifetime"]);
 
 		// database.max_open_connections
-		if (int64_t(doc["database"]["max_open_connections"])<0) {
+		if (static_cast<int64_t>(doc["database"]["max_open_connections"])<0) {
 			throw std::invalid_argument("database.max_open_connections must be 0 or greater");
 		}
-		properties["database_max_open_connections"] = (int)int64_t(doc["database"]["max_open_connections"]);
+		properties["database_max_open_connections"] = static_cast<int64_t>(doc["database"]["max_open_connections"]);
 
 		// database.max_idle_connections
-		if (int64_t(doc["database"]["max_idle_connections"])<0) {
+		if (static_cast<int64_t>(doc["database"]["max_idle_connections"])<0) {
 			throw std::invalid_argument("database.max_idle_connections must be 0 or greater");
 		}
-		properties["database_max_idle_connections"] = (int)int64_t(doc["database"]["max_idle_connections"]);
+		properties["database_max_idle_connections"] = static_cast<int64_t>(doc["database"]["max_idle_connections"]);
 
-	} catch (const simdjson::simdjson_error& e) {
+	} catch (const simdjson::simdjson_error&) {
 		throw;
 	}
 }
