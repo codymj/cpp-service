23447ba2d86ef6702d26689870e6d78c844f28da
codymj <codyj@protonmail.com>
2024-04-27 12:23:28 -0500

    add override

--- src/app.hpp
+++ src/app.hpp
@@ -11,7 +11,7 @@ using Poco::Net::ServerSocket;
 
 class App : public Poco::Util::ServerApplication {
 protected:
-    void initialize(Application& self);
-    void uninitialize();
-    int main(const std::vector<std::string>& args);
+    void initialize(Application& self) override;
+    void uninitialize() override;
+    int main(const std::vector<std::string>& args) override;
 };
