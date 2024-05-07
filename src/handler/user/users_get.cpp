a73eca0e24743406acccafe08a6723a22e2b63ce
codymj <codyj@protonmail.com>
2024-05-07 18:18:48 -0500

    fix

--- src/handler/user/users_get.cpp
+++ src/handler/user/users_get.cpp
@@ -11,8 +11,8 @@ void UsersGetHandler::handleRequest(HTTPServerRequest& /*req*/, HTTPServerRespon
     std::ostream& os = res.send();
     os << "<h1>GET /users</h1>";
     os << "<ul>Users";
-    os << "<li>" << "User0 ID: " << users[0].userId << "</li>";
-    os << "<li>" << "User1 ID: " << users[1].userId << "</li>";
-    os << "<li>" << "User2 ID: " << users[2].userId << "</li>";
+//    os << "<li>" << "User0 ID: " << users[0].userId << "</li>";
+//    os << "<li>" << "User1 ID: " << users[1].userId << "</li>";
+//    os << "<li>" << "User2 ID: " << users[2].userId << "</li>";
     os << "</ul>";
 }
